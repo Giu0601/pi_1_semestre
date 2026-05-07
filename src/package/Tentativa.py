b1f421a8dc4c0d37d987dc67a9246542dfa6ac84
luizadias1107 <luiza.ld.1107@gmail.com>
2026-05-07 12:49:44 -0300

    feat: implementa estrutura Aluno, Tentativa e Casa (tabuleiro)

--- src/package/Tentativa.py
+++ src/package/Tentativa.py
@@ -1,4 +1,5 @@
 from Tarefa import Tarefa
+from Questao import Questao
 
 class Tentativa (Tarefa):
     def __init__(self, nota: float) -> None:
@@ -6,3 +7,5 @@ class Tentativa (Tarefa):
 
     def set_nota(self, nota: float) -> None:
         self.nota = nota
+
+    
